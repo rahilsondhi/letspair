8df7fa7ca9218b80b711e0fb706d61ff4a4841dc
Rahil Sondhi <rahilsondhi@gmail.com>
2013-04-15 10:34:55 -0400

    Working on landing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Hackerhero::Application.routes.draw do
-  # Prototype
-  %w[search].each do |page|
-    get page => "prototype##{page}"
+  # Prototype, for development only
+  if Rails.env.development?
+    %w[search].each do |page|
+      get page => "prototype##{page}"
+    end
   end
 
-  root to: 'pages#home'
+  # root to: 'pages#home'
+
+  # Landing page for designers
+  root to: 'pages#coming_soon'
+  get 'designers-we-need-your-help-with-open-source' => 'pages#designers'
 end
