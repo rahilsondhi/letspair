95ae679a8b161ee178eddc8852af3ea810351e14
Rahil Sondhi <rahilsondhi@gmail.com>
2013-04-22 00:23:50 -0400

    Adds /needs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
+
 Hackerhero::Application.routes.draw do
   # Prototype, for development only
   if Rails.env.development?
-    %w[search request_to_pair sessions register1 register2].each do |page|
+    %w[search request_to_pair sessions register1 register2 needs].each do |page|
       get page => "prototype##{page}"
     end
+
+    # Aliases
+    get '/post-a-need' => 'prototype#register2'
   end
 
   # Landing pages
