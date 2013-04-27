3c61ff61182d4f879002c91134ffee5f66089e5a
Rahil Sondhi <rahilsondhi@gmail.com>
2013-04-27 16:09:22 -0400

    Adds Session assocations, validations, spec

--- app/models/session.rb
+++ app/models/session.rb
@@ -18,6 +18,53 @@
 #
 
 class Session < ActiveRecord::Base
-  belongs_to :user
+  #
+  # Constants
+  #
+
+  VALID_STATES = %w(open waiting scheduled cancelled)
+
+  #
+  # Assocations
+  #
+
+  has_many    :messages,
+              inverse_of: :session
+
+  belongs_to  :user,
+              inverse_of: :sessions
+
+  belongs_to  :partner,
+              class_name: 'User',
+              foreign_key: 'partner_id',
+              inverse_of: :partnered_sessions
+
+  belongs_to  :need,
+              inverse_of: :sessions
+
+  belongs_to  :waiting_for,
+              class_name: 'User',
+              foreign_key: 'waiting_for_id'
+
+  has_and_belongs_to_many :topics
+
+  #
+  # Validations
+  #
+
+  validates :user_id,
+            presence: true
+
+  validates :partner_id,
+            presence: true
+
+  validates :state,
+            presence: true,
+            inclusion: { in: VALID_STATES }
+
+  #
+  # Misc
+  #
+
   attr_accessible :date_suggested_one, :date_suggested_three, :date_suggested_two, :description, :need_id, :partner_id, :state, :waiting_for
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -8,5 +8,5 @@ describe Message do
 
   it { should belong_to(:author).class_name('User') }
   it { should belong_to(:recipient).class_name('User') }
-  it { should belong_to(:session).class_name('User') }
+  it { should belong_to(:session) }
 end
